1b8616f6ee6fd9e3d54e9c8b6d8aadb9922db102
charb29 <ocharb@mac.com>
2025-06-04 11:05:20 -0700

    task: update basic react fields

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 
 const rootElement = document.getElementById('root');
