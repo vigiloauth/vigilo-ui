7b2e05cc3094fef2999c1de631af4748aef62b4e
charb29 <ocharb@mac.com>
2025-06-04 11:05:50 -0700

    task: add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { lazy, Suspense } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Spin } from 'antd';
+import FlexContainer from './components/FlexContainer';
+import ProtectedRoute from './components/ProtectedRoute';
+import { AuthProvider } from './context/AuthContext'; // Add this
+import './index.scss';
+
+const LoginPage = lazy(() => import('./pages/Login/LoginPage'));
+const RegistrationPage = lazy(
+  () => import('./pages/Registration/RegistrationPage')
+);
+const PasswordResetPage = lazy(
+  () => import('./pages/PasswordReset/PasswordResetPage')
+);
+const DashboardPage = lazy(() => import('./pages/Dashboard/DashboardPage'));
+
+const AppRoutes = () => {
+  return (
+    <Suspense
+      fallback={
+        <FlexContainer width="100%" height="100vh">
+          <Spin size="large" />
+        </FlexContainer>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<RegistrationPage />} />
+        <Route path="/reset-password" element={<PasswordResetPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute requireAdmin={true}>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Suspense>
+  );
+};
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </BrowserRouter>
   );
 };
 
