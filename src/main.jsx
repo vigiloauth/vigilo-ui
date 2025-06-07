28dad15d81ead0a1f79c7a4816cd7ea6c15dac59
charb29 <ocharb@mac.com>
2025-06-07 09:54:49 -0700

    style: remove duplicate import statements

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { React, StrictMode } from 'react';
-import { React, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
